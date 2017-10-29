672277c5c7ba49c3c1ef922eaebd8a994aaf0363
Matthew Brent <mattjbrent@gmail.com>
2017-10-29 17:32:09 -0400

    added server side code and production/development builds

---
+++ src/shared/config.js
@@ -0,0 +1,5 @@
+export const WEB_PORT = process.env.PORT || 8000
+// If the Node process used to run your app has a process.env.PORT
+// environment variable set, this will be used || 8000
+export const STATIC_PATH = '/static'
+export const APP_NAME = 'Blank Slate'
