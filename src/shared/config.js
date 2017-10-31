4db13731aa2888da565ca7cd9b16f1f2aa40379a
Matthew Brent <mattjbrent@gmail.com>
2017-10-30 20:11:56 -0400

    added webpack into the mix, added dist folder

--- src/shared/config.js
+++ src/shared/config.js
@@ -1,5 +1,8 @@
 export const WEB_PORT = process.env.PORT || 8000
+export const WDS_PORT = 7000
 // If the Node process used to run your app has a process.env.PORT
 // environment variable set, this will be used || 8000
 export const STATIC_PATH = '/static'
 export const APP_NAME = 'Blank Slate'
+export const APP_CONTAINER_CLASS = 'js-app'
+export const APP_CONTAINER_SELECTOR = `.${APP_CONTAINER_CLASS}`
